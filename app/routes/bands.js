76ce4d9bf811fd4aaac443c992b405acfbe86ac0
Hector Hurtarte <hectorh30@gmail.com>
2015-12-12 13:40:46 -0600

    WIP

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,27 +1,6 @@
 import Ember from 'ember';
-
-var Band = Ember.Object.extend({
-  name: '',
-  language: '',
-
-  slug: Ember.computed('name', function() {
-    console.log('Recomputing slug');
-
-    return this.get('name').dasherize();
-  }),
-
-  site: Ember.computed('slug', 'language', function() {
-    console.log('Recomputing site');
-
-    return 'http://bands.com/' + this.get('slug') + '.' + this.get('language');
-  })
-});
-
-var Song = Ember.Object.extend({
-  title: '',
-  rating: 0,
-  band: ''
-});
+import Band from '../models/band';
+import Song from '../models/song';
 
 var blackDog = Song.create({
   title: 'Black Dog',
